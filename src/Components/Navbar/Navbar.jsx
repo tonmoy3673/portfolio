610b7191c49cbfdbe61826ee0e91f4116540ac5f
tonmoy <tonmoy3673@gmail.com>
2024-06-08 23:28:20 +0600

    :bulb: set logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,14 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className='navbar'>
-            <img src='' alt='logo'/>
+            <img src='/public/assets/logo.svg' alt='logo'/>
+            <ul className='nav-menu'>
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contact</li>
+
+            </ul>
 
         </div>
     );
