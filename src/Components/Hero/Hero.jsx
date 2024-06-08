1443e1ebfcceac815cc7a77f7fba5d4e0b1530fc
tonmoy <tonmoy3673@gmail.com>
2024-06-09 04:11:34 +0600

    :bulb: create button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,10 +2,10 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div className="hero md:flex items-center gap-[30px]">
+        <div className="hero md:flex items-center gap-[30px] justify-center">
             <div>
-            <h1>I am Mehedi Hasan Tonmoy, A passionate Front-End Developer from Bangladesh</h1>
-            <p>I have a ton of prior experience in web development from the Front-End point of view and am well-versed in coding for both Single Page Applications and larger multiple paged Websites.</p>
+            <h1><span className='hero-title text-center'>I am Mehedi Hasan Tonmoy,</span> A passionate Front-End Developer from Bangladesh</h1>
+            <p className='hero-text text-center'>I have a ton of prior experience in web development from the Front-End point of view and am well-versed in coding for both Single Page Applications and larger multiple paged Websites.</p>
             <div className="hero-action">
             <div className="hero-connect">
                 Connect with me
@@ -15,9 +15,9 @@ const Hero = () => {
             </div>
             </div>
             </div>
-            <div>
-            <img src="/public/assets/hero.png" className="w-72 rounded-full mx-auto"/>
-            </div>
+            <figure>
+            <img src="/public/assets/hero.png" className="w-96"/>
+            </figure>
         </div>
     );
 };
