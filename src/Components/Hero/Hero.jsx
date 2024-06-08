c21a3eca0a70f026c10f87750a9342181999c75e
tonmoy <tonmoy3673@gmail.com>
2024-06-09 05:47:37 +0600

    :pencil: set theme image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,9 +2,9 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div className="hero md:flex items-center gap-[30px] justify-center">
+        <div className="hero md:flex items-center gap-[30px] justify-center mx-[170px]">
             <div>
-            <h1><span className='hero-title text-center'>I am Mehedi Hasan Tonmoy,</span> A passionate Front-End Developer from Bangladesh</h1>
+            <h1><span className='hero-title text-center'>I am Mehedi Hasan Tonmoy,</span><br/>A passionate Front-End Developer from Bangladesh</h1>
             <p className='hero-text text-center'>I have a ton of prior experience in web development from the Front-End point of view and am well-versed in coding for both Single Page Applications and larger multiple paged Websites.</p>
             <div className="hero-action">
             <div className="hero-connect">
@@ -15,9 +15,9 @@ const Hero = () => {
             </div>
             </div>
             </div>
-            <figure>
-            <img src="/public/assets/hero.png" className="w-96"/>
-            </figure>
+            
+            <img src="/public/assets/hero.png" className="w-5/12"/>
+            
         </div>
     );
 };
