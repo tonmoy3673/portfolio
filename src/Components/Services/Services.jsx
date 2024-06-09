e27dd80ba6285af177e50f5e4430804cf94dc351
tonmoy <tonmoy3673@gmail.com>
2024-06-09 18:32:26 +0600

    :tada: set services card

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -11,29 +11,29 @@ const Services = () => {
             </div>
             
             <div className="grid grid-cols-1 lg:grid-cols-3 services-container gap-16 justify-center text-center ">
-               <div className='services-format py-12'>
-               <h3>01</h3>
+               <div className='services-format'>
+              
                 <h1>Web Design</h1>
                
-                <div className="services readmore flex">
+                <div className="services-readmore">
                     <p>Read More</p>
                     <img src='/public/assets/arrow_icon.svg'/>
                 </div>
                </div>
                <div className='services-format'>
-               <h3>02</h3>
+              
                 <h1>Web Development</h1>
             
-                <div className="services readmore">
+                <div className="services-readmore">
                     <p>Read More</p>
                     <img src='/public/assets/arrow_icon.svg'/>
                 </div>
                </div>
                <div className='services-format'>
-               <h3>03</h3>
-                <h3>Content Writing</h3>
                
-                <div className="services readmore">
+                <h1>Content Writing</h1>
+               
+                <div className="services-readmore">
                     <p>Read More</p>
                     <img src='/public/assets/arrow_icon.svg'/>
                 </div>
