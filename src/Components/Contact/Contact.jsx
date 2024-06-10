9ca570defcc76bc381e87b1d59668fdf40d6fa60
tonmoy <tonmoy3673@gmail.com>
2024-06-10 18:56:30 +0600

    :bulb: added icons in contact part

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,7 +12,21 @@ const Contact = () => {
                 <div className="contact-left">
                     <h1>Let's talk</h1>
                     <p>I'm currently available to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+                    <div className="contact-details">
+                        <div className="contact-detail">
+                            <img src='/public/assets/mail_icon.svg' alt='email'/> <p>meheditonmoy917@gmail.com</p>
+                        </div>
+                        <div className="contact-detail">
+                        <img src='/public/assets/call_icon.svg' alt='phone'/> <p>+8801966527486</p>
+                        </div>
+                        <div className="contact-detail">
+                        <img src='/public/assets/location_icon.svg'/> <p>Dhaka, Bangladesh</p>
+                        </div>
+                    </div>
                 </div>
+
+                {/* ============ form part =========== */}
+                <form  className="contact-right"></form>
             </div>
             
         </div>
