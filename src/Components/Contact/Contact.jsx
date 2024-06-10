85f66d7050297f2890034092655fea17bda0d6f9
tonmoy <tonmoy3673@gmail.com>
2024-06-10 18:08:13 +0600

    :bulb: added content in contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 
 import './Contact.css';
 const Contact = () => {
@@ -6,6 +7,12 @@ const Contact = () => {
             <div className="contact-title">
             <h1>Get in Touch</h1>
             <img src='/public/assets/theme_pattern.svg'/> 
+            </div>
+            <div className="contact-section">
+                <div className="contact-left">
+                    <h1>Let's talk</h1>
+                    <p>I'm currently available to take on new projects, so feel free to send me a message about anything that you want me to work on. You can contact anytime.</p>
+                </div>
             </div>
             
         </div>
