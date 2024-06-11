dd6d875fdeb5c47040ef033764e93f2d896f76a9
tonmoy <tonmoy3673@gmail.com>
2024-06-11 13:58:36 +0600

    :tada: added icons in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,27 @@
 
+import { FaFacebook, FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa6';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div>
+        <div className='footer'>
+            <div className="footer-top">
+                <div className="footer-top-left">
+                <div className='nav-title'>
+                <h1>Tonmoy</h1>
+                <img src='/public/assets/theme_pattern.svg'/>
+
+            </div>
+            <p>I am Mehedi Hasan Tonmoy,
+            A passionate Front-End Developer from Bangladesh</p>
+                </div>
+                <div className="footer-top-right">
+                   <a href='https://www.facebook.com/mehedihasan.tonmoy.7161/' target='_blank'> <FaFacebook/></a>
+                   <a href='https://github.com/tonmoy3673' target='_blank'> <FaGithub/></a>
+                   <a href='https://www.linkedin.com/in/mehedi-hasan-tonmoy/' target='_blank'> <FaLinkedin/></a>
+                   
+                </div>
+            </div>
             
         </div>
     );
