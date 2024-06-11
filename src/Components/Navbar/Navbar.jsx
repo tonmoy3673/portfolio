4feb302698e2886741ed36a092dc668338fd882f
tonmoy <tonmoy3673@gmail.com>
2024-06-11 15:46:16 +0600

    :bulb: set small images in navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [menu,setMenu]=useState('home')
     return (
         <div className='navbar md:pt-2'>
             {/* <img src='/public/assets/logo.svg' alt='logo'/> */}
@@ -10,10 +12,11 @@ const Navbar = () => {
 
             </div>
             <ul className='nav-menu'>
-                <li className='hover:text-[#da7c25]'>Home</li>
-                <li className='hover:text-[#da7c25]'>About Me</li>
-                <li className='hover:text-[#da7c25]'>Projects</li>
-                <li className='hover:text-[#da7c25]'>Contact</li>
+                <li className='hover:text-[#da7c25]'><p onClick={()=>setMenu('home')}>Home</p>{menu==='home'?<img src='/public/assets/nav_underline.svg' alt='underline'/>:<></>}</li>
+                <li className='hover:text-[#da7c25]'><p onClick={()=>setMenu('about')}>About Me</p>{menu==='about'?<img src='/public/assets/nav_underline.svg'/>:<></>}</li>
+                <li className='hover:text-[#da7c25]'><p onClick={()=>setMenu('project')}>Projects</p>{menu==='project'?<img src='/public/assets/nav_underline.svg'/>:<></>}</li>
+                <li className='hover:text-[#da7c25]'><p onClick={()=>setMenu('services')}>Services</p>{menu==='services'?<img src='/public/assets/nav_underline.svg'/>:<></>}</li>
+                <li className='hover:text-[#da7c25]'><p onClick={()=>setMenu('contact')}>Contact</p>{menu==='contact'?<img src='/public/assets/nav_underline.svg'/>:<></>}</li>
 
             </ul>
             <div className="nav-connect">
