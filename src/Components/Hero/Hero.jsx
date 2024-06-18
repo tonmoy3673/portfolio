b8333a50d5fc17a799677c1a64750cefa84751b3
tonmoy <tonmoy3673@gmail.com>
2024-06-18 21:38:02 +0600

    :tada: design hero button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div id='home' className="hero md:flex items-center gap-[16px] lg:gap-[30px] justify-center lg:mx-[170px]">
+        <div id='home' className="hero md:flex items-center md:gap-[30px] justify-center lg:mx-[170px]">
             <div className='order-2 md:order-1 '>
             <h1><span className='hero-title text-center'>I am Mehedi Hasan Tonmoy,</span><br/>A passionate Front-End Developer from Bangladesh</h1>
             <p className='hero-text text-center'>I have a ton of prior experience in web development from the Front-End point of view and am well-versed in coding for both Single Page Applications and larger multiple paged Websites.</p>
@@ -17,7 +17,7 @@ const Hero = () => {
             </div>
             </div>
             
-            <img  src="/public/assets/hero.png" className="w-1/3 md:w-5/12 order-1 md:order-2"/>
+            <img  src="/public/assets/hero.png" className="w-1/3 md:w-5/12 order-1 md:order-2 !mx-auto md:mx-0"/>
             
         </div>
     );
