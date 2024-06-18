bb7e0dbdf7658b76f39b402be827033ce7c45e54
tonmoy <tonmoy3673@gmail.com>
2024-06-18 21:49:05 +0600

    :tada: design hero button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div id='home' className="hero md:flex items-center md:gap-[30px] justify-center lg:mx-[170px]">
+        <div id='home' className="hero md:flex items-center md:gap-[30px] justify-center text-center md:text-start lg:mx-[170px]">
             <div className='order-2 md:order-1 '>
             <h1><span className='hero-title text-center'>I am Mehedi Hasan Tonmoy,</span><br/>A passionate Front-End Developer from Bangladesh</h1>
             <p className='hero-text text-center'>I have a ton of prior experience in web development from the Front-End point of view and am well-versed in coding for both Single Page Applications and larger multiple paged Websites.</p>
